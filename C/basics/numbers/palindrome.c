47418f07af545daed1ed9d76a3f0aeb11376b117
ishandas-dev <ishanmbdas@gmail.com>
2026-05-04 22:59:48 +0530

    Palindrome number using C

--- C/basics/numbers/palindrome.c
+++ C/basics/numbers/palindrome.c
@@ -1,15 +1,16 @@
+/* To check the given number is palindrome number or not using C*/
 #include<stdio.h>
 int main()
 {
     int n,d=0,rev=0,temp=0;
     printf("Enter the number \n");
     scanf("%d",&n);
-    temp=n;
+    temp=n; //using temporary variable to save the original number 
     while(temp>0)
     {
-        d=temp%10;
-        rev=rev*10+d;
-        temp=temp/10;
+        d=temp%10; //extracting the last digit of the number
+        rev=(rev*10)+d; //reversing the number
+        temp=temp/10; //removing the last digit
     }
     if (n==rev)
     printf("the given number %d is a palindrome \n",n);
@@ -17,4 +18,3 @@ int main()
     printf("the given number %d is not a palindrome \n",n);
     return 0;
 }
-    
