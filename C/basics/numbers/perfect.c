cfe997f27fdfe812ec1e204a73e57126066e9b97
ishandas-dev <ishanmbdas@gmail.com>
2026-05-08 23:43:50 +0530

    To check number is perfect number or not

--- C/basics/numbers/perfect.c
+++ C/basics/numbers/perfect.c
@@ -1,9 +1,11 @@
+/*To check the given number is perfect number or not using C*/
 #include<stdio.h>
 int main()
 {
     int n,i,sum=0;
     printf("Enter the value of number \n");
     scanf("%d",&n);
+    //to store the sum of all the numbers which are divisble to the given number
     for(i=1;i<n;i++)
     {
         if(n%i==0)
