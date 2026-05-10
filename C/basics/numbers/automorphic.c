d91640719169e3fcfffd9da617eaf6d0d68258ab
ishandas-dev <ishanmbdas@gmail.com>
2026-05-10 22:36:40 +0530

    To check number is perfect number or not

--- C/basics/numbers/automorphic.c
+++ C/basics/numbers/automorphic.c
@@ -1,22 +1,28 @@
+/*To check the given number is automorphic number or not using C*/
 #include<stdio.h>
 int main()
 {
-    int n,temp=0,digit,check=1,i,square;
+    int n,temp=0,digit=0,check=1,i,square;
     printf("Enter the value of number \n");
     scanf("%d",&n);
-    temp=n;
+    temp=n; //using temporary variable to store the given number
     square=n*n;
+
+    //counting how many digits are there in the number
     while(temp>0)
     {
-        digit++
+        digit++;
         temp=temp/10;
     }
+
+    /*To get 10^digits so that we can extract the required data 
+    from square to check automorphic number*/
     for(i=0;i<digit;i++)
     {
         check=check*10;
     }
     if(square%check==n)
-    printf("The given number %d is an automorphic number \n",n);
+    printf("The given number %d is an automorphic number as it's square is %d\n",n,square);
     else
-    printf("The given number %d is not an automorphic number \n",n);
+    printf("The given number %d is not an automorphic number as it's square is %d\n",n,square);
 }
