4f676e2744a2b1fe957c103f2edb53ee2ca70f53
ishandas-dev <ishanmbdas@gmail.com>
2026-05-29 00:39:21 +0530

    Printing Geometric series and sum

--- C/basics/series/GeometricSeries.c
+++ C/basics/series/GeometricSeries.c
@@ -1,5 +1,10 @@
+/*A geometric series is the sum of the terms in a geometric sequence (a list of numbers where 
+each term is multiplied by a fixed common ratio).
+For example : the sequence 2, 6, 18, 54, 162 is a geometric sequence with a common ratio of 
+3 . When you add them together 2 + 6 + 18 + 54 + 162 = 242 , it becomes a geometric series.*/
+
 #include<stdio.h>
-#include<math.h>
+#include<math.h> //including this header file to use pow function
 int main()
 {
     int a=0,r=0,n=0,i;
@@ -10,11 +15,13 @@ int main()
     scanf("%d",&a);
     printf("Enter the common ratio of the series \n");
     scanf("%d",&r);
-    for(i=a;i<=a*pow(r,n-1);i=i*r)
+
+    //using loop to print the Geometric series
+    for(i=a;i<=a*pow(r,n-1);i=i*r) //loop from first term to last term, increment by multiplying the common ratio
     {
-         printf("%d ",i);
+         printf("%d ",i); 
     }
-    sum= (a*(pow(r,n)-1.0))/(r-1.0);
-    printf("\nThe sum of this geometric series is %f",sum);
+    sum= (a*(pow(r,n)-1.0))/(r-1.0); //calculating the sum of Geometric series by using its formula
+    printf("\nThe sum of this geometric series is %f",sum); 
     return 0;
 }
