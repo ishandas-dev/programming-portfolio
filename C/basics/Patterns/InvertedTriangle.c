af3bc8c3c441382b2e33ab9df394f0d749e02d87
ishandas-dev <ishanmbdas@gmail.com>
2026-06-01 22:11:29 +0530

    Printing the inverted right triangle

--- C/basics/Patterns/InvertedTriangle.c
+++ C/basics/Patterns/InvertedTriangle.c
@@ -1,12 +1,17 @@
+/*An inverted right triangle pattern in programming is a geometric shape made of characters (like * or numbers) 
+where the widest part is at the top, and the rows progressively decrease in size to form a point pointing downwards.*/
+
 #include<stdio.h>
 int main()
 {
     int i,j,row=0;
     printf("Enter the number \n");
     scanf("%d",&row);
+
+    //printing the pattern
     for(i=1;i<=row;i++)
     {
-        for(j=row;j>=i;j--)
+        for(j=row;j>=i;j--) //using loop from given row's value till the value of i variable, j decrement with every loop
         {
             printf("* ");
         }
