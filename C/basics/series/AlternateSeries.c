2bdf7f59bc550ac1a21fa5d16760e3a0742a285d
ishandas-dev <ishanmbdas@gmail.com>
2026-06-02 22:41:09 +0530

    Printing the alternate series and sum till the given number

--- C/basics/series/AlternateSeries.c
+++ C/basics/series/AlternateSeries.c
@@ -7,14 +7,26 @@ int main()
     int n,i,sum=0;
     printf("Enter the value of n \n");
     scanf("%d",&n);
+    printf("The series looks like this :");
     for(i=1;i<=n;i++)
     {
-        if(i%2==0)
-        sum=sum-i;
-        else
+        if(i==1) 
+        {
+        printf(" %d",i); //printing the first number of the series
         sum=sum+i;
+        }
+        else if(i%2==0) //checking for even and odd
+        {
+        printf(" - %d",i); //printing the series
+        sum=sum-i; //calculating the sum
+        }
+        else
+        {
+        printf(" + %d",i); //printing the series
+        sum=sum+i; //calculating the sum
+        }
     }
-    printf("The sum of Alternate series for given number %d is %d",n,sum);
+    printf("\nThe sum of Alternate series for given number %d is %d \n",n,sum);
     return 0;
 }
 
