31f5517135a4f7484121d290cefdf30d7a1ccde6
ishandas-dev <ishanmbdas@gmail.com>
2026-06-03 22:52:24 +0530

    Printing Square series and sum

--- C/basics/series/SquareSeries.c
+++ C/basics/series/SquareSeries.c
@@ -1,5 +1,5 @@
 /*A square number series is a mathematical sequence where each term is the product of an 
-integer multiplied by itself, expressed as \(n^{2}\).
+integer multiplied by itself, expressed as (n^2).
  The sequence begins (1, 4, 9, 16, 25, 36, 49, 64, 81, 100 till n)*/
 
 #include<stdio.h>
@@ -8,11 +8,23 @@ int main()
     int n,i,square=0;
     printf("Enter the value till which the series to be printed\n");
     scanf("%d",&n);
+
+    //using loop for squaring the numbers
     for(i=1;i<=n;i++)
     {
-        square= i*i; //squaring the number
-        printf("%d ",square);
-        square=0;//again initializing the variable to 0 so we can use it again
+        if(i==1)
+        {
+            square=i*i; //squaring the starting number
+            printf("%d",square); //printing the square
+            square=0; //initializing the variable to 0 so we can use it again
+        }
+        else
+        {
+        square= i*i; //squaring the numbers
+        printf(" , %d",square); //printing the square
+        square=0; //again initializing the variable to 0 so we can use it again
+        }
     }
     return 0;
 }
+//you might wonder why i am squaring 1 separately , its because i want to print "," too after each number
