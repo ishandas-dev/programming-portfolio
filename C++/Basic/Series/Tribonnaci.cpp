95ab9765ffa8cb57d890803026aed9c93799b42e
ishandas-dev <ishanmbdas@gmail.com>
2026-07-21 18:32:51 +0530

    Printing the Tribonnaci series

---
+++ C++/Basic/Series/Tribonnaci.cpp
@@ -0,0 +1,24 @@
+/*Floyd's triangle is a right-angled triangular array of consecutive natural numbers. Named after 
+computer scientist Robert W. Floyd, it is built by filling the rows with incremental numbers from
+ left to right, where the first row has 1 element, the second has 2, and so on.
+ the series looks like : The series typically starts with the initial values (0,0,1,1,2,4,7,13,24,44,81,149,274 till n)*/
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a=0,b=0,c=1,d=0,i,num=0;
+    cout << "Enter the value of n \n";
+    cin >> num;
+
+    //using loop tp print the Tribonacci series
+    for(i=1;i<=num;i++)
+    {
+        cout << a;  // print current term
+        d=a+b+c;    // calculate next term (sum of previous 3)
+        a=b;        // shift a to next position
+        b=c;        // shift b to next position
+        c=d;        // shift c to next position
+    }
+    return 0;
+}
