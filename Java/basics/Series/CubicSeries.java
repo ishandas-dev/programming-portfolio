67204b9db00507903a6e90991843604ae3197d01
ishandas-dev <ishanmbdas@gmail.com>
2026-08-07 23:43:41 +0530

    Printing the CubicSeries using java

---
+++ Java/basics/Series/CubicSeries.java
@@ -0,0 +1,22 @@
+/*In mathematics and reasoning tests, a cube series (or cubic sequence) is a sequence of numbers
+ in which each term is found by taking the cube of its position in the series. 
+ This means a number is multiplied by itself, and then by itself again (n*n*n or n^3)
+ The sequence begins (1,8 27 64 125 216 343 512 729 1000 till n )*/
+
+import java.util.*;
+public class CubicSeries
+{
+    public static void main(String args[])
+    {
+        Scanner in= new Scanner(System.in);
+        int cube=0,num,i;
+        System.out.println("Enter the value of of number ");
+        num=in.nextInt();
+        for(i=1;i<=num;i++)
+        {
+            cube=i*i*i;//cubing the number
+            System.out.println(+cube);
+            cube=0;//again initializing the variable to 0 so we can use it again
+        }
+    }
+}
