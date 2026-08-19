351ec83c2f58c92d4ae7251f9c6b334e009bd8b0
ishandas-dev <ishanmbdas@gmail.com>
2026-08-19 22:13:46 +0530

    Learning to use malloc,calloc and realloc

--- C/Intermediate/DynamicMemoryAllocation/Learning-Malloc.c
+++ C/Intermediate/DynamicMemoryAllocation/Learning-Malloc.c
@@ -1,3 +1,8 @@
+/*Write a C program to demonstrate dynamic memory allocation using malloc(), calloc(), and 
+realloc(). Take the size and elements as input from the user for each case, and display the 
+allocated values. Free the allocated memory appropriately, and check thereturn value of each 
+allocation function to handle allocation failure safely.*/
+
 #include<stdio.h>
 #include<stdlib.h>
 int main()
@@ -8,7 +13,12 @@ int main()
     //using malloc
     printf("Enter the value of n for malloc \n");
     scanf("%d",&n);
-    ptr=(int*)malloc(n*sizeof(int));
+    ptr=(int*)malloc(n*sizeof(int));   
+    if(ptr==NULL)
+    {
+        printf("Memory allocation failed (Malloc)\n");
+        return 1;
+    }
     printf("Enter the elements for malloc \n");
     for(i=0;i<n;i++)
     {
@@ -19,12 +29,17 @@ int main()
     {
         printf("At %d index the value stored is %d \n",i,ptr[i]);
     }
-    free(ptr);
+    free(ptr);  //freeing the the ptr before using it below
 
     //using Calloc
     printf("Enter the value of n for calloc \n");
     scanf("%d",&n);
     ptr=(int*)calloc(n,sizeof(int));
+    if(ptr==NULL)
+    {
+        printf("Memory allocation failed (calloc)\n");
+        return 1;
+    }
     printf("Enter the elements for calloc \n");
     for(i=0;i<n;i++)
     {
@@ -39,8 +54,15 @@ int main()
     //using Realloc
     printf("Enter the value of new n you want to create with realloc\n");
     scanf("%d",&n);
-    ptr=(int*)realloc(ptr,n*sizeof(int));
-     printf("Enter the elements for realloc\n");
+    int* temp = (int*)realloc(ptr,n*sizeof(int));   //storing result in temp first,so ptr could not be overwrite
+    if(temp==NULL)
+    {
+        printf("Memory allocation failed (realloc)\n");
+        free(ptr);    //realloc failed,so the original block is still valid and must be freed 
+        return 1;
+    }
+    ptr=temp;    //safe to update ptr only after confirming realloc succeeded
+    printf("Enter the elements for realloc\n");
     for(i=0;i<n;i++)
     {
         scanf("%d",&ptr[i]);
