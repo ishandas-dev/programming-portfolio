2990b55235e5bebc9681cc8a11ccaf0305577177
ishandas-dev <ishanmbdas@gmail.com>
2026-09-14 22:04:17 +0530

    Reverse triangle pattern

--- C++/Basic/Patterns/ReverseTrianglePattern1.cpp
+++ C++/Basic/Patterns/ReverseTrianglePattern1.cpp
@@ -1,3 +1,11 @@
+/*Take a number 'row' as input. Print an inverted triangle pattern where each row prints numbers 
+counting down from the row number to 1.
+ Example: row = 4
+   1
+   2 1
+   3 2 1
+   4 3 2 1 */
+
 #include<iostream>
 using namespace std;
 int main()
@@ -5,9 +13,9 @@ int main()
     int i,j,row=0;
     cout << "Enter the number of rows\n";
     cin >> row;
-    for(i=1;i<=row;i++)
+    for(i=1;i<=row;i++)  //outer loop runs for each row
     {
-        for(j=i;j>=1;j--)
+        for(j=i;j>=1;j--)  // inner loop counts down from i to 1
         {
             cout << j << " ";
         }
